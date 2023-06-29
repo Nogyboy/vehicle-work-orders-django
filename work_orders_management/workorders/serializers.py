a0668b5e7c9559c6f2e8684c86d04959ca5ab4fa
Nogyboy <guamanc9@gmail.com>
2023-06-29 11:17:46 -0500

    Get work orders active

--- work_orders_management/workorders/serializers.py
+++ work_orders_management/workorders/serializers.py
@@ -1,25 +1,13 @@
 from rest_framework import serializers
-from .models import Vehicle
+from vehicles.models import Vehicle
+from .models import WorkOrders
+from vehicles.serializers import ClientVehiclesSerializer
 
-class ClientVehiclesSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Vehicle
-        fields = ['id', 'plate', 'brand', 'model', 'year', 'color', 'image']
 
-class CreateVehicleSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Vehicle
-        fields = ['plate', 'brand', 'model', 'year', 'color', 'image']
 
-class UpdateVehicleSerializer(serializers.ModelSerializer):
-    plate = serializers.CharField(max_length=10, required=False)
+class VehicleWorkOrdersActiveSerializer(serializers.ModelSerializer):
+    vehicle = ClientVehiclesSerializer(read_only=True)
 
     class Meta:
-        model = Vehicle
-        fields = '__all__'
-
-    def get_fields(self):
-        fields = super().get_fields()
-        if self.partial and 'plate' not in self.initial_data:
-            fields['plate'].required = False
-        return fields
+        model = WorkOrders
+        fields = ['id', 'vehicle', 'start_date', 'end_date', 'description', 'status']
