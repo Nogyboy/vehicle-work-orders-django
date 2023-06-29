03cd6a69cce6bd2bc1cc34aa70d8884e3a093a70
Nogyboy <guamanc9@gmail.com>
2023-06-28 20:36:02 -0500

    User role base permissions

--- work_orders_management/authentication/serializers.py
+++ work_orders_management/authentication/serializers.py
@@ -1,7 +1,15 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 from .models import CustomUser
 
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['username', 'email']
+
 class CustomUserSerializer(serializers.ModelSerializer):
+    user = UserSerializer(read_only=True)
+
     class Meta:
         model = CustomUser
-        fields = ['id', 'username', 'email', 'is_cliente', 'is_administrador']
+        fields = ['user', 'is_cliente', 'is_administrador']
--- work_orders_management/authentication/views.py
+++ work_orders_management/authentication/views.py
@@ -5,11 +5,24 @@ from rest_framework.decorators import api_view, permission_classes, authenticati
 from rest_framework.response import Response
 from rest_framework import status
 
+
+class IsAdminOrReadOnly(permissions.BasePermission):
+    def has_permission(self, request, view):
+        # Verificar si el usuario autenticado es un administrador
+        print(request.user.customuser.is_administrador)
+        print(request.user.customuser.is_cliente)
+        if request.user.is_authenticated and request.user.customuser.is_administrador:
+            return True
+        
+        # Permitir solo métodos GET para usuarios no autenticados o no administradores
+        return False
+
 @api_view(['GET'])
-@permission_classes([permissions.IsAuthenticated])
+@permission_classes([permissions.IsAuthenticated, IsAdminOrReadOnly])
 def custom_user_list(request):
     queryset = CustomUser.objects.all()
     serializer = CustomUserSerializer(queryset, many=True)
+    
     return Response(serializer.data)
 
 @api_view(['GET'])
